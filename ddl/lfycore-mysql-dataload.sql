a29806b3db3394ecd43ad0f66a94c5274e58be87
lililiu <lililiu@promote.cache-dns.local>
2014-04-03 16:34:20 +0800

    新建好基础Nutz框架，完成简单登录、退出功能

---
+++ ddl/lfycore-mysql-dataload.sql
@@ -0,0 +1,15 @@
+--tb_user
+insert into tb_user(name,passwd,status) values('admin','admin','1');
+
+
+----
+INSERT INTO account VALUES('j2ee','yourname@yourdomain.com','ABC', 'XYX', 'OK', '901 San Antonio Road', 'MS UCUP02-206', 'Palo Alto', 'CA', '94303', 'USA',  '555-555-5555');
+INSERT INTO account VALUES('ACID','acid@yourdomain.com','ABC', 'XYX', 'OK', '901 San Antonio Road', 'MS UCUP02-206', 'Palo Alto', 'CA', '94303', 'USA',  '555-555-5555');
+
+INSERT INTO bannerdata VALUES ('FISH','<image src="../images/banner_fish.gif">');
+INSERT INTO bannerdata VALUES ('CATS','<image src="../images/banner_cats.gif">');
+
+INSERT INTO category VALUES ('FISH','Fish','<image src="../images/fish_icon.gif"><font size="5" color="blue"> Fish</font>');
+INSERT INTO category VALUES ('DOGS','Dogs','<image src="../images/dogs_icon.gif"><font size="5" color="blue"> Dogs</font>');
+
+
---
+++ src/com/liufuya/core/bean/User.java
@@ -0,0 +1,57 @@
+package com.liufuya.core.bean;
+
+import org.nutz.dao.entity.annotation.Name;
+import org.nutz.dao.entity.annotation.Table;
+import org.nutz.dao.entity.annotation.Id;
+
+@Table("tb_user")
+public class User {
+
+	@Id
+	private long id;
+	
+	//唯一
+	@Name
+	private String name;
+	
+	//设置为 瞬时变量
+	private transient String passwd;
+
+	private String status;
+	
+	public User(){}
+
+	public User(String name, String passwd, String status) {
+		super();
+		this.name = name;
+		this.passwd = passwd;
+		this.status = status;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	
+	
+	
+}
